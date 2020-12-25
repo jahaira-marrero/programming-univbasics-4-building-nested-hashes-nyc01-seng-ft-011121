c78243e0b1af78159c120b9a20dd6357d2bb4f49
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-12-25 17:45:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,16 +37,15 @@ if monopoly_with_third_tier[:railroads][:names]
     :b_and_o_railroad => {},
     :shortline_railroad => {}
 }
-
 end
 end
 monopoly_with_third_tier
 end
 
-# def monopoly_with_fourth_tier
+def monopoly_with_fourth_tier
 #   # When you start writing the implementation for this method, copy the Hash
 #   # you built in the previous method. Run the tests. They will fail, but
 #   # they'll guide you in how to modify what you just did to have more
 #   # complexity.
 
-# end
+end
